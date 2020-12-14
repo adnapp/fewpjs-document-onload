af61b5759a9053cf7e93897dcce56c9cbabbc4c6
adnapp <adam.napps@gmail.com>
2020-12-14 17:54:15 -0500

    commit tree 268ce973f634c14ee35812d92ab5b1aa93f20786

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDom();
+  });
+
+  function updateDom () { 
+      paragraph = document.querySelector("p#text") 
+    //   debugger
+      paragraph.textContent =  "This really cool!"
+  };
